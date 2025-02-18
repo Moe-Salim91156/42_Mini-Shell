3a37d04e253185dd78cd023870d1c92c23eb27bd
Your Namegit config --global user.email you@example.com  git config --global user.name Your <ykitaneh22@gmail.com>
2025-02-18 14:05:30 +0300

    more merge resolving

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,14 +6,13 @@
 /*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 19:12:28 by msalim            #+#    #+#             */
-/*   Updated: 2025/02/18 13:09:44 by yokitane         ###   ########.fr       */
+/*   Updated: 2025/02/18 13:50:52 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 # include "../libft/libft.h"
-# include "envp.h"
 # include "builtins.h"
 # include <unistd.h>
 # include <ctype.h>
@@ -41,8 +40,8 @@ typedef struct s_token
 
 typedef struct s_token_list
 {
-	int				size;
-	t_token			*head;
+	int		size;
+	t_token	*head;
 }					t_token_list;
 
 typedef struct s_cmd
@@ -53,13 +52,13 @@ typedef struct s_cmd
 
 typedef struct s_cmd_list
 {
-	int				count;
-	t_cmd			*head;
+	int		count;
+	t_cmd	*head;
 }					t_cmd_list;
 
 typedef struct s_envp
 {
-	char	*value;
+	char			*value;
 	struct s_envp	*next;
 }	t_envp;
 
@@ -95,6 +94,6 @@ int				bltn_cd(char *dst);
 int				bltn_echo(char **args);
 int				bltn_export(char **args);
 int				bltn_unset(char **args);
-int				bltn_env(void);
+void			bltn_env(void);
 int 			bltn_exit(int status);
 #endif
---
+++ srcs/parser/token_utils.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 18:01:06 by msalim            #+#    #+#             */
+/*   Updated: 2025/02/18 13:49:56 by yokitane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+int	is_seperator_token(char c)
+{
+	return (c == ' ' || c == '|' || c == '\n');
+}
+
+int	is_quotes(char c)
+{
+	return (c == '\'' || c == '\"');
+}
+
+int	is_redirect(char c)
+{
+	return (c == '>' || c == '<');
+}
+
+void	add_last_token(char *input, int start, int i, t_token_list *tokens)
+{
+	char	*result;
+
+	if (start < i)
+	{
+		result = ft_substr(input, start, i - start);
+		add_token(tokens, result);
+		free(result);
+	}
+}
+
+void	add_token(t_token_list *list, char *value)
+{
+	t_token	*new_node;
+	t_token	*temp;
+
+	new_node = init_token();
+	if (!new_node)
+		return ;
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+	if (!list->head)
+		list->head = new_node;
+	else
+	{
+		temp = list->head;
+		while (temp->next)
+			temp = temp->next;
+		temp->next = new_node;
+	}
+	list->size++;
+}
