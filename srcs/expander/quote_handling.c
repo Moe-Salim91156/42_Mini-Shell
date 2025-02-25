199dd4650e2510c4307db76fc6cd61e6eceb7444
Moe-Salim91156 <hmodaisleem88@gmail.com>
2025-02-25 15:09:11 +0300

    norminette

--- srcs/expander/quote_handling.c
+++ srcs/expander/quote_handling.c
@@ -20,42 +20,42 @@ int	is_single_quote_token(char *token)
 		return (0);
 }
 
-int check_quotes_even_odd(char *value, char quote)
+int	check_quotes_even_odd(char *value, char quote)
 {
-  int i;
-  int start_quote;
-  int end_quote;
+	int	i;
+	int	start_quote;
+	int	end_quote;
 
-  i = 0;
-  start_quote= 0;
-  end_quote = ft_strlen(value) - 1;
-   while (value[i] == quote)
-  {
-    start_quote++;
-    i++;
-  } 
-   if ((start_quote + end_quote) % 2 == 0)
-    return (0);
-   else
-     return (1);
+	i = 0;
+	start_quote = 0;
+	end_quote = ft_strlen(value) - 1;
+	while (value[i] == quote)
+	{
+		start_quote++;
+		i++;
+	}
+	if ((start_quote + end_quote) % 2 == 0)
+		return (0);
+	else
+		return (1);
 }
 
 char	*extract_quotes(char *value, char quote)
 {
-  int i;
-  int j;
+	int		i;
+	int		j;
 	char	*result;
 
 	i = 0;
-  j = 0;
+	j = 0;
 	result = malloc(ft_strlen(value) + 1);
-	if (!result || !check_quotes_even_odd(value,quote))
+	if (!result || !check_quotes_even_odd(value, quote))
 		return (NULL);
 	while (value[i])
 	{
 		if (value[i] != quote)
 		{
-      ft_strncpy(&result[j],&value[i],1);
+			ft_strncpy(&result[j], &value[i], 1);
 			j++;
 		}
 		i++;
@@ -75,11 +75,11 @@ void	check_for_quotes_in_tokens(t_token_list *list)
 		if (is_single_quote_token(current->value))
 		{
 			new_value = extract_quotes(current->value, '\'');
-      if (new_value)
-      {
-			  free(current->value);
-			  current->value = new_value;
-      }
+			if (new_value)
+			{
+				free(current->value);
+				current->value = new_value;
+			}
 		}
 		current = current->next;
 	}
