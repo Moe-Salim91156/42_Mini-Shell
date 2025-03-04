bc235a49b062a083567f801fb75129e426b079ce
dotacow <ykitaneh22@gmail.com>
2025-03-04 03:19:22 +0300

    unset done, cd should be done, will begin with executor for now

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 10:10:49 by yokitane          #+#    #+#             */
-/*   Updated: 2025/03/04 02:57:55 by yokitane         ###   ########.fr       */
+/*   Updated: 2025/03/04 03:07:14 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,22 @@
 	0 if directory changed successfuly, >0 otherwise.
 	######
 	TBD:
-		handle too many arguments.
+		properly handle no args
+		properly handle too many args
+		(will have to see how executer
+		passes args first)
 */
 
 int	bltnc_cd(char *path, t_envp *list)
 {
 	char oldpwd[1024];
 
-	if (chdir(path) == -1)
+	if (!*path && (chdir(find_by_key(list, "HOME=")->value) == -1))//placeholder stuff
+	{
+			perror("cd: ");
+			return (1);
+	}
+	else if (chdir(path) == -1)
 	{
 		perror("cd: ");
 		return (1);
