da45a925b6bbd29b5aa9025f933e8781938db3c0
dotacow <ykitaneh22@gmail.com>
2025-04-03 11:54:00 +0300

    exit.c

--- srcs/builtins/bltn_exit.c
+++ srcs/builtins/bltn_exit.c
@@ -6,7 +6,7 @@
 /*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 03:13:13 by yokitane          #+#    #+#             */
-/*   Updated: 2025/03/27 02:03:35 by yokitane         ###   ########.fr       */
+/*   Updated: 2025/04/03 11:53:46 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,41 @@
 	of the most recently executed command, and if at anypoint
 	------
 	tbd: an exit handler to free heap and close open fds.
+	-----
+	a thought I had in mind: should  make exit prompt a
 */
+
+static int is_numeric(const char *str)
+{
+	if (*str ==
+
+	'+' || *str == '-')
+		str++;
+    while (*str)
+    {
+        if (!ft_isdigit(*str))
+            return (1);
+        str++;
+    }
+    return (0);
+}
+
+static unsigned long ft_atoul(char *str)
+{
+	unsigned long ret;
+
+	ret = 0;
+	if (*str == '+' || *str == '-')
+		str++;
+	while (*str && ft_isdigit(*str))
+	{
+		ret = ret * 10 + (*str - '0');
+		str++;
+	}
+	return (ret);
+}
+
+int	bltn_exit(char **args, t_shell *shell);
+{
+}
+
