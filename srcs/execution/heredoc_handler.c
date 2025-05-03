1864ed37fb1c5e737dcc0ad3c07942ebd03689bf
dotacow <dotacowidkmynamesry@gmail.com>
2025-05-03 19:44:58 +0300

    fix: norm

--- srcs/execution/heredoc_handler.c
+++ srcs/execution/heredoc_handler.c
@@ -6,7 +6,7 @@
 /*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 21:15:37 by yokitane          #+#    #+#             */
-/*   Updated: 2025/05/01 17:33:41 by msalim           ###   ########.fr       */
+/*   Updated: 2025/05/03 19:44:48 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,92 +22,92 @@
 
 void heredoc_read_loop(t_cmd *p, char **envp, int write_fd)
 {
-    char *in;
-    while (1)
-    {
-        in = readline("> ");
-        if (!in || !ft_strcmp(in, p->heredoc_delimiter))
-        {
-            free(in);
-            break;
-        }
-        if (!p->heredoc_quoted)
-            in = expand_heredoc_line(in, envp);
-        write(write_fd, in, ft_strlen(in));
-        write(write_fd, "\n", 1);
-        free(in);
-    }
+	char *in;
+	while (1)
+	{
+		in = readline("> ");
+		if (!in || !ft_strcmp(in, p->heredoc_delimiter))
+		{
+			free(in);
+			break;
+		}
+		if (!p->heredoc_quoted)
+			in = expand_heredoc_line(in, envp);
+		write(write_fd, in, ft_strlen(in));
+		write(write_fd, "\n", 1);
+		free(in);
+	}
 }
 
 int run_heredoc(t_cmd *p, t_shell *s, char **envp)
 {
   (void)s;
-    int fd[2];
-    if (pipe(fd) < 0)
-        return -1;
-    heredoc_read_loop(p, envp, fd[1]);
-    close(fd[1]);
-    return fd[0];
+	int fd[2];
+	if (pipe(fd) < 0)
+		return -1;
+	heredoc_read_loop(p, envp, fd[1]);
+	close(fd[1]);
+	return fd[0];
 }
 
 void process_heredoc_helper(t_cmd *cmd, t_shell *shell, int *i, char **envp)
 {
-    cmd->has_heredoc = 1;
-    if (cmd->payload_array[*i + 1] && cmd->type[*i + 1] == HEREDOC_DELIMITER)
-        cmd->heredoc_delimiter = ft_strdup(cmd->payload_array[*i + 1]);
-    if (cmd->heredoc_fd != -1)
-        close(cmd->heredoc_fd);
-    cmd->heredoc_fd = run_heredoc(cmd, shell, envp);
-    printf("heredoc fd is %d\n",cmd->heredoc_fd);
+	cmd->has_heredoc = 1;
+	if (cmd->payload_array[*i + 1] && cmd->type[*i + 1] == HEREDOC_DELIMITER)
+		cmd->heredoc_delimiter = ft_strdup(cmd->payload_array[*i + 1]);
+	if (cmd->heredoc_fd != -1)
+		close(cmd->heredoc_fd);
+	cmd->heredoc_fd = run_heredoc(cmd, shell, envp);
+	printf("heredoc fd is %d\n",cmd->heredoc_fd);
 }
 
 int process_heredocs(t_cmd *cmd, t_shell *shell)
 {
-    int i;
-    int heredoc_count;
-    char **envp;
+	int i;
+	int heredoc_count;
+	char **envp;
 
-    heredoc_count = 0;
-    envp = build_envp(shell);
-    if (!envp)
-        return -1;
-    i = 0;
-    while (cmd->payload_array[i])
-    {
-        if (!ft_strcmp(cmd->payload_array[i], "<<"))
-        {
-            process_heredoc_helper(cmd, shell, &i, envp);
-            if (cmd->heredoc_fd < 0)
-            {
-                free_split(envp);
-                return -1;
-            }
-            heredoc_count++;
-            i += 2;
-        }
-        else
-            i++;
-    }
-    free_split(envp);
-    return heredoc_count;
+	heredoc_count = 0;
+	envp = build_envp(shell);
+	if (!envp)
+		return -1;
+	i = 0;
+	while (cmd->payload_array[i])
+	{
+		if (!ft_strcmp(cmd->payload_array[i], "<<"))
+		{
+			process_heredoc_helper(cmd, shell, &i, envp);
+			if (cmd->heredoc_fd < 0)
+			{
+				free_split(envp);
+				return -1;
+			}
+			heredoc_count++;
+			i += 2;
+		}
+		else
+			i++;
+	}
+	free_split(envp);
+	return heredoc_count;
 }
 
 int process_all_heredocs(t_shell *shell)
 {
-    t_cmd *current;
-    int total_heredocs;
-    int result;
+	t_cmd *current;
+	int total_heredocs;
+	int result;
 
-    total_heredocs = 0;
-    current = shell->cmd_list->head;
-    while (current)
-    {
-        result = process_heredocs(current, shell);
-        if (result < 0)
-            return -1;
-        total_heredocs += result;
-        current = current->next;
-    }
-    return total_heredocs;
+	total_heredocs = 0;
+	current = shell->cmd_list->head;
+	while (current)
+	{
+		result = process_heredocs(current, shell);
+		if (result < 0)
+			return -1;
+		total_heredocs += result;
+		current = current->next;
+	}
+	return total_heredocs;
 }
 
