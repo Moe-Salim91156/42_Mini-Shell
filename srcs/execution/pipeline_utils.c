7e37761ef0a1a113c8b830fed9e5644732582574
dotacow <ykitaneh22@gmail.com>
2025-05-11 13:16:43 +0300

    norm

--- srcs/execution/pipeline_utils.c
+++ srcs/execution/pipeline_utils.c
@@ -6,12 +6,11 @@
 /*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 19:17:42 by yokitane          #+#    #+#             */
-/*   Updated: 2025/05/10 20:03:15 by yokitane         ###   ########.fr       */
+/*   Updated: 2025/05/11 13:15:45 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-#include <stdio.h>
 
 /*
 	safe closure of pipes.
