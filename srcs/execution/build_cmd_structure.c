6f390af01dce1c7d739b7e447f8cf85e1e77ac4e
Moe-Salim91156 <hmodaisleem88@gmail.com>
2025-05-12 19:15:59 +0300

    last last last

--- srcs/execution/build_cmd_structure.c
+++ srcs/execution/build_cmd_structure.c
@@ -6,7 +6,7 @@
 /*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 17:47:33 by msalim            #+#    #+#             */
-/*   Updated: 2025/05/10 16:43:04 by yokitane         ###   ########.fr       */
+/*   Updated: 2025/05/12 19:13:36 by msalim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ char	**malloc_cmd_argv(t_cmd *payload)
 	int		args_num;
 
 	args_num = count_args(payload);
-	argv = malloc(sizeof(char *) * (args_num + 1));
+	argv = malloc(sizeof(char *) * (args_num + 3));
 	if (!argv)
 		return (NULL);
 	argv[args_num] = NULL;
@@ -51,6 +51,20 @@ char	**malloc_cmd_argv(t_cmd *payload)
  *
  */
 
+static int nop_case(t_cmd *cmd)
+{
+	int i;
+
+	i = 0;
+	while (cmd->payload_array[i])
+	{
+		if (cmd->type[i] == COMMAND)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 static int	fill_cmd_argv(t_cmd *cmd)
 {
 	int		i;
@@ -61,6 +75,12 @@ static int	fill_cmd_argv(t_cmd *cmd)
 	j = 0;
 	while (cmd->payload_array[i])
 	{
+		if ((!i) && nop_case(cmd) && (cmd->type[1] == FILE_TOKEN ||
+				cmd->type[1] == HEREDOC_DELIMITER))
+		{
+			cmd->argv[j++] = ft_strdup("NOPXRBSH");
+			break;
+		}
 		if (cmd->type[i] == COMMAND || cmd->type[i] == ARGS)
 		{
 			arg = ft_strdup(cmd->payload_array[i]);
@@ -88,37 +108,3 @@ char	**build_cmd_argv(t_cmd_list *list)
 	}
 	return (list->head->argv);
 }
-
-/*
-char	**build_payload_argv(t_cmd *payload)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	payload->argv = malloc_cmd_argv(payload);
-	if (!payload->argv)
-		return (NULL);
-	while (payload->payload_array[i])
-	{
-		if (payload->type[i] == COMMAND && ft_strcmp(payload->payload_array[i],
-				" "))
-		{
-			payload->argv[0] = ft_strdup(payload->payload_array[i]);
-			break ;
-		}
-		i++;
-	}
-	while (payload->payload_array[i])
-	{
-		if (payload->type[i] == ARGS)
-		{
-			payload->argv[j] = ft_strdup(payload->payload_array[i]);
-			j++;
-		}
-		i++;
-	}
-	payload->argv[j] = NULL;
-	return (payload->argv);
-}*/
